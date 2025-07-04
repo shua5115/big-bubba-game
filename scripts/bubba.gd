ee27ac047f90219363abfea700b3f60b836b5296
zach geyer <zachg@debianlinuxz.zachgeyer.net>
2025-07-03 23:30:10 -0400

    player_movement

--- scripts/bubba.gd
+++ scripts/bubba.gd
@@ -1,3 +1,64 @@
 extends CharacterBody3D
 
+@export var arm1 : Node3D
+@export var arm2 : Node3D
+
+@export var camera_holder : Node3D
+
+@export var camera : Camera3D
+
+var arm_off_pos = -1
+
+var time : float = 0
+
 # TODO
+func _ready():
+	#arm_off_pos = arm1.position.z
+	pass
+	
+func _process(delta):
+	time += 0.01
+	arm1.position.y = sin(time*10)*0.2 + arm_off_pos
+	arm2.position.y = sin(time*10)*0.2 + arm_off_pos
+
+	pass
+
+@export var SPEED : float = 5.0
+@export var JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
+@export var gravity : float
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			camera_holder.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction = (camera_holder.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
